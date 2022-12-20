85d346a0545839ea7dad67e45ac964c6310d7c16
Rui Afonso <afonso.r.dev@gmail.com>
2022-12-20 17:00:21 +0000

    Add Express.static method, error handling

--- index.js
+++ index.js
@@ -4,21 +4,29 @@ const express = require('express'),
 const app = express();
 
 app.use(morgan('common'));
-app.use('/documentation.html', express.static('public'));
+app.use(express.static('public'));
+
+let topTenMovies = [];
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my app!');
+  res.send('Welcome to myFlix APP!');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topTenMovies);
 });
 
 app.get('/documentation', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
+  res.sendFile('public/documentation.html', { root: __dirname });
 });
 
+// ERROR Handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
 
+//Listen for requests
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080');
 });
