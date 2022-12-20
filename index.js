ae0125167c73fd5bc6a24d7cd7f84135ed61a157
Rui Afonso <afonso.r.dev@gmail.com>
2022-12-20 16:27:26 +0000

    Create new directory and delete log file

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-const express = require('express');
+const express = require('express'),
+  morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan('common'));
+app.use('/documentation.html', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my app!');
+});
+
+app.get('/documentation', (req, res) => {
+  res.send('This is a secret url with super top-secret content.');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080');
+});
