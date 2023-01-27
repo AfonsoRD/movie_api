880d39bd8f9ddc1eca153cb6aed4805e07fba9ed
Rui Afonso <afonso.r.dev@gmail.com>
2023-01-27 13:22:39 +0100

    remove authentication method in movies endpoint

--- index.js
+++ index.js
@@ -50,20 +50,16 @@ app.get('/', (req, res) => {
 //Return JSON object when at /movies
 
 //GET all movies
-app.get(
-  '/movies',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Movies.find()
-      .then((movies) => {
-        res.status(200).json(movies);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-      });
-  }
-);
+app.get('/movies', (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(200).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 //GET movies By Title
 app.get(
